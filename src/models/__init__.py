6e0c4a1fb51665072c0a70ba5f5a58d9727a0d53
diegowh <contacto.diegowh@gmail.com>
2024-08-05 17:02:24 +0200

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,17 @@
-from sqlalchemy.orm import DeclarativeBase
-
-
-class Base (DeclarativeBase):
-    pass
+from src.models.account.account import Account
+from src.models.league.league_entry import LeagueEntry
+from src.models.match.ban import Ban
+from src.models.match.challenges import Challenges
+from src.models.match.info import Info
+from src.models.match.match import Match
+from src.models.match.metadata import Metadata
+from src.models.match.missions import Missions
+from src.models.match.objective import Objective
+from src.models.match.objectives import Objectives
+from src.models.match.participant import Participant
+from src.models.match.perk_stats import PerkStats
+from src.models.match.perk_style_selection import PerkStyleSelection
+from src.models.match.perk_style import PerkStyle
+from src.models.match.perks import Perks
+from src.models.match.team import Team
 
