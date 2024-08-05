1649c5b3b605d29a29c9d27efd5fd60a0db123e4
diegowh <contacto.diegowh@gmail.com>
2024-08-05 17:04:32 +0200

    Create Info model

--- src/models/match/info.py
+++ src/models/match/info.py
@@ -1,24 +1,31 @@
 from __future__ import annotations
-from dataclasses import dataclass
+from src.models.base import Base
+from sqlalchemy import Mapped, true
+from sqlalchemy.orm import Mapped, mapped_column
 from src.models.match.participant import Participant
 from src.models.match.team import Team
 
 
-@dataclass
-class Info:
-    endOfGameResult: str
-    gameCreation: int
-    gameDuration: int
-    gameEndTimestamp: int
-    gameId: int
-    gameMode: str
-    gameName: str
-    gameStartTimestamp: int
-    gameType: str
-    gameVersion: str
-    mapId: int
+
+class Info(Base):
+
+    __tablename__ = "info"
+    
+    id: Mapped[int] = mapped_column(primary_key=true, autoincrement=true, index=true, unique=true)
+    endOfGameResult: Mapped[str]
+    gameCreation: Mapped[int]
+    gameDuration: Mapped[int]
+    gameEndTimestamp: Mapped[int]
+    gameId: Mapped[int]
+    gameMode: Mapped[str]
+    gameName: Mapped[str]
+    gameStartTimestamp: Mapped[int]
+    gameType: Mapped[str]
+    gameVersion: Mapped[str]
+    mapId: Mapped[int]
+    platformId: Mapped[str]
+    queueId: Mapped[int]
+    tournamentCode: Mapped[str]
+
     participants: list[Participant]
-    platformId: str
-    queueId: int
     teams: list[Team]
-    tournamentCode: str
