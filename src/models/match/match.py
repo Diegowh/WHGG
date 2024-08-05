5e2f7a139b052b877a5c025d6aa903d5cbfca6d8
diegowh <contacto.diegowh@gmail.com>
2024-08-05 17:04:39 +0200

    Create Match model

--- src/models/match/match.py
+++ src/models/match/match.py
@@ -1,10 +1,16 @@
 from __future__ import annotations
+from src.models.base import Base
+from sqlalchemy import Mapped, true
+from sqlalchemy.orm import Mapped, mapped_column
 from src.models.match.metadata import Metadata
 from src.models.match.info import Info
-from dataclasses import dataclass
 
 
-@dataclass
-class Match:
-    metadata: Metadata
-    info: Info
+
+class Match(Base):
+    
+    __tablename__ = "match"
+
+    id: Mapped[int] = mapped_column(primary_key=true, unique=true, autoincrement=true, index=true)
+    metadata: Mapped[Metadata]
+    info: Mapped[Info]
