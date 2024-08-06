56c94cde1067d08b722d26a17bdf94731d1acfb0
diegowh <contacto.diegowh@gmail.com>
2024-08-06 12:54:54 +0200

    Update metadata.py

--- src/models/match/metadata.py
+++ src/models/match/metadata.py
@@ -1,12 +1,16 @@
 from __future__ import annotations
-from typing import Optional, List, Union
 from src.models.account.account import Account
-from dataclasses import dataclass
+from src.models.base import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer
 
 
-@dataclass
-class Metadata:
-    dataVersion: str
-    matchId: str
-    participants: Union[str]
-    
+
+class Metadata(Base):
+    __tablename__ = "metadata"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    match_id: Mapped
+    dataVersion: Mapped[str]
+    matchId: Mapped[str]
+    participants: Mapped[list[str]]
