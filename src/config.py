0628b24da93f62be6bb2427876c031c7dea9f917
diegowh <contacto.diegowh@gmail.com>
2024-08-15 15:58:31 +0200

    Add DB attr

--- src/config.py
+++ src/config.py
@@ -3,6 +3,15 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     app_name: str = "WHGG"
+
+    # RiotGames API
     RIOT_API_KEY: str
+
+    # Database
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: str
 
     model_config = SettingsConfigDict(env_file='.env')
