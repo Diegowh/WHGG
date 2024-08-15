9e1c3346c14603df6293877584fadd5422bb0fbd
diegowh <contacto.diegowh@gmail.com>
2024-08-15 15:59:15 +0200

    Update routes

---
+++ src/database/models/__init__.py
@@ -0,0 +1,7 @@
+from src.database.models.account import Account
+from src.database.models.base import Base
+from src.database.models.champion_stats import ChampionStats
+from src.database.models.league_entry import LeagueEntry
+from src.database.models.match import Match
+from src.database.models.participant import Participant
+from src.database.models.version import Version
