dccdc54d5c5c4273544d86ff245d5aa578b14a3b
diegowh <contacto.diegowh@gmail.com>
2024-08-15 16:00:54 +0200

    refactor: move models into /database

--- src/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from src.models.account import Account
-from src.models.base import Base
-from src.models.champion_stats import ChampionStats
-from src.models.league_entry import LeagueEntry
-from src.models.match import Match
-from src.models.participant import Participant
-from src.models.version import Version
