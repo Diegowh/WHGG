25d10c74b73de8105a244883f9b0b583b2123b6a
diegowh <contacto.diegowh@gmail.com>
2024-08-16 14:31:52 +0200

    Update database.py

--- src/database/database.py
+++ src/database/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from src.config import Settings
 
 
@@ -9,7 +8,16 @@ settings = Settings()
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+# El propósito de sessionmaker es de proveer una fábrica para objetos Session con una configuración fija.
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def init_db():
+    """_summary_
+    """
