36da67e696db727c8d098f272f07783146b27286
diegowh <contacto.diegowh@gmail.com>
2024-08-16 14:32:04 +0200

    Remove old import

--- src/database/models/__init__.py
+++ src/database/models/__init__.py
@@ -1,5 +1,4 @@
 from src.database.models.account import Account
-from src.database.models.base import Base
 from src.database.models.champion_stats import ChampionStats
 from src.database.models.league_entry import LeagueEntry
 from src.database.models.match import Match
