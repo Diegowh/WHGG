e8299b8e0903aab09d43f7c42ada366978acd099
diegowh <contacto.diegowh@gmail.com>
2024-08-18 15:03:44 +0200

    Update __init__.py

--- backend/database/schemas/__init__.py
+++ backend/database/schemas/__init__.py
@@ -3,3 +3,4 @@ from backend.database.schemas.champion_stats import *
 from backend.database.schemas.league_entry import *
 from backend.database.schemas.match import *
 from backend.database.schemas.participant import *
+from backend.database.schemas.request import *
