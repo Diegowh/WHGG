323275402c68e4e49bbffeb925020cbefbe87d51
diegowh <contacto.diegowh@gmail.com>
2024-08-21 14:25:06 +0200

    remove autocommit and autoflush

--- backend/database/database.py
+++ backend/database/database.py
@@ -6,7 +6,7 @@ from backend.config import settings
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(engine)
 
 
 class Base(DeclarativeBase):
