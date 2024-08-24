2055508a9bc20c0b003cdeace18e6ce8ac4b2cef
diegowh <contacto.diegowh@gmail.com>
2024-08-24 15:13:04 +0200

    Fix pylint warnings and errors

--- backend/database/models/account.py
+++ backend/database/models/account.py
@@ -1,3 +1,8 @@
+'''
+Este módulo contiene la clase `Account` que extiende de `sqlalchemy.orm.DeclarativeBase`
+y representa un modelo de tabla
+'''
+
 from typing import Optional
 
 from sqlalchemy import String, UniqueConstraint
@@ -8,16 +13,32 @@ from backend.database.database import Base
 from backend.database.models.champion_stats import ChampionStats
 from backend.database.models.league_entry import LeagueEntry
 from backend.database.models.match import Match
-    
+
 
 class Account(Base):
+    """Representa un modelo de Account en la base de datos.
+
+    Esta clase extiende de `sqlalchemy.orm.DeclarativeBase`
+    y se mapea a la tabla `account` en la base de datos.
+
+    La clase `Account` contiene información sobre una cuenta de Riot Games.
+    """
     __tablename__ = "account"
-    
+
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
 
-    league_entries: Mapped[list["LeagueEntry"]] = relationship(back_populates="account", cascade="all, delete-orphan")
-    matches: Mapped[list["Match"]] = relationship(back_populates="account", cascade="all, delete-orphan")
-    champion_stats: Mapped[list["ChampionStats"]] = relationship(back_populates="account", cascade="all, delete-orphan")
+    league_entries: Mapped[list["LeagueEntry"]] = relationship(
+        back_populates="account",
+        cascade="all, delete-orphan"
+    )
+    matches: Mapped[list["Match"]] = relationship(
+        back_populates="account",
+        cascade="all, delete-orphan"
+    )
+    champion_stats: Mapped[list["ChampionStats"]] = relationship(
+        back_populates="account",
+        cascade="all, delete-orphan"
+    )
 
     puuid: Mapped[str] = mapped_column(String(78), unique=True, index=True)
     summoner_id: Mapped[str] = mapped_column(String(63), unique=True, index=True)
@@ -31,11 +52,19 @@ class Account(Base):
     __table_args__ = (
         UniqueConstraint("game_name", "tag_line", name="_game_name_tag_line_uc"),
     )
-    
+
     def __repr__(self) -> str:
-        return (f"Account(puuid={self.puuid!r}, summonerId={self.summoner_id!r}, "
-                f"accountId={self.account_id!r}, gameName={self.game_name!r}, "
-                f"tagLine={self.tag_line!r}, profileIconId={self.profile_icon_id!r}, "
-                f"lastUpdate={self.last_update!r}, summonerLevel={self.summoner_level!r}, "
-                f"league_entries={self.league_entries!r}, matches={self.matches!r},"
-                f"champion_stats={self.champion_stats!r}")
+        attrs = [
+            f"puuid={self.puuid!r}",
+            f"summonerId={self.summoner_id!r}",
+            f"accountId={self.account_id!r}",
+            f"gameName={self.game_name!r}",
+            f"tagLine={self.tag_line!r}",
+            f"profileIconId={self.profile_icon_id!r}",
+            f"summonerLevel={self.summoner_level!r}",
+            f"lastUpdate={self.last_update!r}",
+            f"league_entries=[{', '.join(repr(le) for le in self.league_entries)}]",
+            f"matches=[{', '.join(repr(m) for m in self.matches)}]",
+            f"champion_stats=[{', '.join(repr(cs) for cs in self.champion_stats)}]"
+        ]
+        return f"Account({', '.join(attrs)})"
