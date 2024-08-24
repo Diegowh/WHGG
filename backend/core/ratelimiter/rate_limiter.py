e635f6abf66f5da74b32b56de5e89fdac3dc9bdb
diegowh <contacto.diegowh@gmail.com>
2024-08-24 12:03:05 +0200

    Rename methods

--- backend/core/ratelimiter/rate_limiter.py
+++ backend/core/ratelimiter/rate_limiter.py
@@ -19,7 +19,7 @@ class RateLimitedClient(httpx.Client):
         self.last_reset = self.clock()
         super().__init__(**kwargs)
     
-    def _reset_if_needed(self):
+    def _reset_request_count(self):
         current_time = self.clock()
         elapsed_time = current_time - self.last_reset
         
@@ -27,9 +27,9 @@ class RateLimitedClient(httpx.Client):
             self.requests_made = 0 
             self.last_reset = current_time
     
-    def _acquire(self):
+    def _wait_for_slot(self):
         while True:
-            self._reset_if_needed()
+            self._reset_request_count()
             if self.requests_made < self.count:
                 self.requests_made += 1
                 break
@@ -39,10 +39,10 @@ class RateLimitedClient(httpx.Client):
                 if time_to_wait > 0:
                     print(f"Rate limit reached. Waiting for {time_to_wait:.2f} seconds...")
                     time.sleep(time_to_wait)
-                self._reset_if_needed()
+                self._reset_request_count()
     
     @wraps(httpx.Client.send)
     def send(self, *args, **kwargs):
-        self._acquire()
+        self._wait_for_slot()
         return super().send(*args, **kwargs)
     
