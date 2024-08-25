7dd4d2f6360972491aac1c6adb04846de9139693
diegowh <contacto.diegowh@gmail.com>
2024-08-25 19:04:05 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -9,6 +9,10 @@ from fastapi import FastAPI
 from backend.api.v1.endpoints import router as api_router
 from backend.database.database import init_db
 
-app = FastAPI(title="WHGG")
+app = FastAPI(
+    title="WHGG",
+    summary="API para obtener los datos de usuarios de League of Legends.",
+    version="1.0",
+)
 init_db()
 app.include_router(api_router)
