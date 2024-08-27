2e78d826b15b81c735c0b514e66b4b40101af541
diegowh <contacto.diegowh@gmail.com>
2024-08-28 01:53:29 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,27 @@
 import "./App.css";
-import ProfileImage from "./components/ProfileImage";
+import ProfileImage from "./components/Profile/ProfileImage";
 // import UpdateButton from "./components/UpdateButton";
-import FiddlesticksIcon from "./assets/static/6022.png";
+import FiddlesticksIcon from "./assets/6022.png";
+import SearchBar from "./components/SearchBar/SearchBar";
+import { Box } from "@chakra-ui/react";
+import Home from "./pages/Home";
 
 function App() {
   const level: number = 534;
   const imageSrc: string = FiddlesticksIcon;
   const imageAlt: string = "Fiddlesticks";
+  function handleSearch(params: { query: string; server: string }) {
+    console.log("Search query:", params.query);
+    console.log("Selected server:", params.server);
+  }
 
-  return <ProfileImage src={imageSrc} alt={imageAlt} level={level} />;
+  return (
+    // <Box p={5}>
+    //   <SearchBar onSearch={handleSearch} />
+    //   <ProfileImage src={imageSrc} alt={imageAlt} level={level} />
+    // </Box>
+    <Home onSearch={handleSearch} />
+  );
 }
 
 export default App;
