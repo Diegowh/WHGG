9f4f1e9b85268191551213da1dbc2ccb189a879e
diegowh <contacto.diegowh@gmail.com>
2024-08-27 19:48:18 +0200

    Add middlewares

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,7 @@ from fastapi import FastAPI
 
 from backend.api.v1.endpoints import router as api_router
 from backend.database.database import init_db
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="WHGG",
@@ -15,4 +16,11 @@ app = FastAPI(
     version="1.0",
 )
 init_db()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(api_router)
