5e751a1d0d1397634a7d2efcee95920eacb53393
diegowh <contacto.diegowh@gmail.com>
2024-08-29 03:18:25 +0200

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { Box, Image } from "@chakra-ui/react";
 import SearchBar from "../components/SearchBar/SearchBar";
-import logo from "../assets/wh-logo.png";
+import logo from "../assets/whgg-logo.png";
 
 function Home() {
   return (
@@ -11,8 +11,8 @@ function Home() {
       alignItems="center"
       justifyContent="center"
     >
-      <Image src={logo} alt="Logo" mb={8} />
-      <SearchBar />
+      <Image src={logo} alt="Logo" mb={8} width="300px" />
+      <SearchBar width="50%" height="10%" />
     </Box>
   );
 }
