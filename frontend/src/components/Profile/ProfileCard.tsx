48a0011683761ca351005c36b30376d3516f66dc
diegowh <contacto.diegowh@gmail.com>
2024-08-29 12:18:40 +0200

    Refactor: Create Profilecard

---
+++ frontend/src/components/Profile/ProfileCard.tsx
@@ -0,0 +1,83 @@
+import { Box, Button, Text, useTheme } from "@chakra-ui/react";
+import ProfileImage from "./ProfileImage";
+import FiddlesticksIcon from "../../assets/6022.png";
+import FiddlesticksBackground from "../../assets/fiddlesticks_bg.jpg";
+
+type ProfileCardProps = {
+  level?: number;
+  imageSrc?: string;
+  imageAlt?: string;
+  bgImage?: string;
+};
+
+function ProfileCard({
+  level = 534,
+  imageSrc = FiddlesticksIcon,
+  imageAlt = "Fiddlesticks",
+  bgImage = FiddlesticksBackground,
+}: ProfileCardProps) {
+  //   const level: number = 534;
+  //   const imageSrc: string = FiddlesticksIcon;
+  //   const imageAlt: string = "Fiddlesticks";
+  //   const bgImage: string = FiddlesticksBackground;
+
+  const theme = useTheme();
+  const terciaryColor = theme.colors.terciary;
+  return (
+    <Box
+      // bgColor={"tomato"}
+      display={"flex"}
+      position={"relative"}
+      alignItems={"center"}
+      justifyContent={"flex-start"}
+      p={4}
+      _before={{
+        content: '""',
+        position: "absolute",
+        top: "0%",
+        right: "0%",
+        bottom: "0%",
+        left: "0%",
+        backgroundImage: `linear-gradient(to left, rgba(0, 0, 0, 0), rgba(7, 7, 32, 1)80%), url(${bgImage})`,
+
+        backgroundPosition: "100% 25%",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "80%",
+        opacity: 0.6,
+        zIndex: -1,
+      }}
+    >
+      <ProfileImage src={imageSrc} alt={imageAlt} level={level} />
+      <Box
+        // bgColor={"tomato"}
+        display="flex"
+        flexDirection="column"
+        alignItems="start"
+      >
+        <Text
+          // as="b"
+          // backgroundColor={"tomato"}
+          fontSize="3xl"
+          position="relative"
+          pt={6}
+          fontFamily={"Barlow, sans-serif"}
+        >
+          wallhack #1312
+        </Text>
+        <Button
+          mt={1}
+          mb={4}
+          bgColor={terciaryColor}
+          borderRadius={3}
+          color={"white"}
+          width={150}
+          _hover={{ bg: { terciaryColor } }}
+        >
+          Update
+        </Button>
+      </Box>
+    </Box>
+  );
+}
+
+export default ProfileCard;
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,18 +1,9 @@
-import { Box, Button, Text, useTheme } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import HeaderBar from "../components/HeaderBar";
-import ProfileImage from "../components/Profile/ProfileImage";
-import FiddlesticksIcon from "../assets/6022.png";
-import FiddlesticksBackground from "../assets/fiddlesticks_bg.jpg";
+
+import ProfileCard from "../components/Profile/ProfileCard";
 
 function Dashboard() {
-  const level: number = 534;
-  const imageSrc: string = FiddlesticksIcon;
-  const imageAlt: string = "Fiddlesticks";
-  const bgImage: string = FiddlesticksBackground;
-
-  const theme = useTheme();
-  const secondaryColor = theme.colors.secondary;
-  const terciaryColor = theme.colors.terciary;
   return (
     <Box display="flex" flexDirection="column" height="100vh">
       <HeaderBar />
@@ -28,59 +19,7 @@ function Dashboard() {
           borderRadius={5}
           width="1000px"
         >
-          <Box
-            // bgColor={"tomato"}
-            display={"flex"}
-            position={"relative"}
-            alignItems={"center"}
-            justifyContent={"flex-start"}
-            p={4}
-            _before={{
-              content: '""',
-              position: "absolute",
-              top: "0%",
-              right: "0%",
-              bottom: "0%",
-              left: "0%",
-              backgroundImage: `linear-gradient(to left, rgba(0, 0, 0, 0), rgba(7, 7, 32, 1)80%), url(${bgImage})`,
-
-              backgroundPosition: "100% 25%",
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "90%",
-              opacity: 0.6,
-              zIndex: -1,
-            }}
-          >
-            <ProfileImage src={imageSrc} alt={imageAlt} level={level} />
-            <Box
-              // bgColor={"tomato"}
-              display="flex"
-              flexDirection="column"
-              alignItems="start"
-            >
-              <Text
-                // as="b"
-                // backgroundColor={"tomato"}
-                fontSize="3xl"
-                position="relative"
-                pt={6}
-                fontFamily={"Barlow, sans-serif"}
-              >
-                wallhack #1312
-              </Text>
-              <Button
-                mt={1}
-                mb={4}
-                bgColor={terciaryColor}
-                borderRadius={3}
-                color={"white"}
-                width={150}
-                _hover={{ bg: { terciaryColor } }}
-              >
-                Update
-              </Button>
-            </Box>
-          </Box>
+          <ProfileCard />
         </Box>
       </Box>
     </Box>
