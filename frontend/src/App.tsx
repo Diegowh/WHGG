65bbaef3f44618b859a025bd1ed8aec743f9c55c
diegowh <contacto.diegowh@gmail.com>
2024-08-29 03:17:56 +0200

    Refactor: create dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,11 @@
 import "./App.css";
-import ProfileImage from "./components/Profile/ProfileImage";
-// import UpdateButton from "./components/UpdateButton";
-import FiddlesticksIcon from "./assets/6022.png";
-import SearchBar from "./components/SearchBar/SearchBar";
-import { Box } from "@chakra-ui/react";
+import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 
 function App() {
-  const level: number = 534;
-  const imageSrc: string = FiddlesticksIcon;
-  const imageAlt: string = "Fiddlesticks";
-  function handleSearch(params: { query: string; server: string }) {
-    console.log("Search query:", params.query);
-    console.log("Selected server:", params.server);
-  }
-
   return (
-    // <Box p={5}>
-    //   <SearchBar onSearch={handleSearch} />
-    //   <ProfileImage src={imageSrc} alt={imageAlt} level={level} />
-    // </Box>
-    <Home onSearch={handleSearch} />
+    // <Home/>
+    <Dashboard />
   );
 }
 
