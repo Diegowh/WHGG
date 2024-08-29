0cf11a3e5fe55f92f9798f1e496d08644ea9546f
diegowh <contacto.diegowh@gmail.com>
2024-08-29 03:18:20 +0200

    Update SearchBar.tsx

--- frontend/src/components/SearchBar/SearchBar.tsx
+++ frontend/src/components/SearchBar/SearchBar.tsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import {
   Box,
+  BoxProps,
   Input,
   InputGroup,
   InputRightElement,
   Select,
   IconButton,
   useTheme,
+  InputProps,
+  SelectProps,
 } from "@chakra-ui/react";
 import { Search2Icon } from "@chakra-ui/icons";
 import { useSearch } from "../../hooks/useSearch";
@@ -36,15 +39,27 @@ type OptionProps = {
 function Option({ value }: OptionProps) {
   return (
     <option
-      style={{ backgroundColor: "#2D3748", color: "white" }}
+      style={{ backgroundColor: "#0D0D28", color: "#CDDCFE" }}
       value={value}
     >
       {value}
     </option>
   );
 }
+interface SearchBarProps extends BoxProps {
+  inputProps?: InputProps;
+  selectProps?: SelectProps;
+  width?: string | number;
+  height?: string | number;
+}
 
-function SearchBar() {
+function SearchBar({
+  // width = "100%",
+  // height = "25%",
+  inputProps,
+  selectProps,
+  ...boxProps
+}: SearchBarProps) {
   const [query, setQuery] = useState("");
   const [server, setServer] = useState("EUW");
 
@@ -63,45 +78,52 @@ function SearchBar() {
     <Box
       display="flex"
       alignItems="center"
-      bg={secondaryColor}
-      p={4}
-      borderRadius={20}
+      bg="#0D0D28"
+      borderRadius={5}
+      height={"40px"}
+      {...boxProps}
     >
       <Select
         value={server}
         onChange={(e) => setServer(e.target.value)}
-        width="120px"
-        mr={2}
-        bg="background"
-        color="text"
-        _hover={{ bg: "gray.600" }}
+        width="90px"
+        border="none"
+        mr={0}
+        bg={"#0D0D28"}
+        color="#CDDCFE"
+        _hover={{ bg: "#0D0D28" }}
         _focus={{ boxShadow: "none" }}
         borderRadius="md"
+        // marginLeft="3"
       >
         {servers.map((server) => (
           <Option key={server} value={server} />
         ))}
       </Select>
 
-      <InputGroup>
+      <InputGroup flex="1" ml="0">
         <Input
           placeholder={placeholderText}
           value={query}
           onChange={(e) => setQuery(e.target.value)}
-          bg="white"
-          color="background"
+          bg={"#0D0D28"}
+          color="white"
+          borderRadius="md"
+          border="none"
+          focusBorderColor={secondaryColor}
         />
         <InputRightElement pr="3">
           <IconButton
             aria-label="Search"
             icon={<Search2Icon />}
-            borderRadius={20}
-            height="80%"
+            borderRadius="md"
+            // height="80%"
             onClick={onSearch}
-            bg="background"
+            bg={"#0D0D28"}
             color="white"
-            _hover={{ bg: secondaryColor }}
+            _hover={{ bg: "#0D0D28" }}
             isLoading={isLoading}
+            border="none"
           />
         </InputRightElement>
       </InputGroup>
