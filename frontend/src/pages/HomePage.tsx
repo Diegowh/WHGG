d12035316a666a2f01e86c127fe8efb007eaf2d5
diegowh <contacto.diegowh@gmail.com>
2024-09-05 23:01:42 +0200

    Update HomePage.tsx

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,8 +1,12 @@
 import { Flex, Image } from "@chakra-ui/react";
 import SearchBar from "../components/SearchBar/SearchBar";
 import logo from "../assets/whgg-logo.png";
+import { useSearch } from "../hooks/useSearch";
+import { useNavigate } from "react-router-dom";
 
 export function HomePage() {
+  const { handleSearch, error, isLoading, result } = useSearch();
+
   return (
     <Flex
       height="70vh"
@@ -11,7 +15,12 @@ export function HomePage() {
       justifyContent="center"
     >
       <Image src={logo} alt="Logo" mb={8} width="300px" />
-      <SearchBar width="50%" height="10%" />
+      <SearchBar
+        handleSearch={handleSearch}
+        isLoading={isLoading}
+        width="50%"
+        height="10%"
+      />
     </Flex>
   );
 }
