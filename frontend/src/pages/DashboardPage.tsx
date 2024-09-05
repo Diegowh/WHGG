f23a15d9d7f8027493698baf4c0ee74a32bc5c8f
diegowh <contacto.diegowh@gmail.com>
2024-09-05 17:34:31 +0200

    Update DashboardPage.tsx

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -3,16 +3,19 @@ import HeaderBar from "../components/HeaderBar";
 
 import { ProfileCard } from "../components/Profile/ProfileCard";
 import DashboardContent from "../components/DashboardContent";
+import { useSearch } from "../hooks/useSearch";
 
 export function DashboardPage() {
+  const { handleSearch, error, isLoading, result } = useSearch();
+
   return (
     <Flex direction="column" height="auto" bgColor={"backgound"}>
-      <HeaderBar />
+      <HeaderBar handleSearch={handleSearch} isLoading={isLoading} />
       {/* Dashboard Content */}
       <Flex overflow="auto" flex="1" justifyContent="center">
         <Box borderRadius={5} width="1000px">
-          <ProfileCard />
-          <DashboardContent />
+          <ProfileCard data={result} />
+          <DashboardContent data={result} />
         </Box>
       </Flex>
     </Flex>
