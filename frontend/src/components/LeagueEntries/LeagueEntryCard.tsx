92b4f7eb94d86534cb00bed7b0512820a996fc6e
diegowh <contacto.diegowh@gmail.com>
2024-09-05 17:35:00 +0200

    Update LeagueEntryCard.tsx

--- frontend/src/components/LeagueEntries/LeagueEntryCard.tsx
+++ frontend/src/components/LeagueEntries/LeagueEntryCard.tsx
@@ -3,12 +3,12 @@ import { CardTitle } from "../ui/CardTitle";
 
 interface LeagueEntryCardProps {
   title: string;
-  emblemUrl: string;
-  rank: string;
-  leaguePoints: number;
-  wins: number;
-  losses: number;
-  wr: number;
+  emblemUrl?: string;
+  rank?: string;
+  leaguePoints?: number;
+  wins?: number;
+  losses?: number;
+  wr?: number;
 }
 
 function LeagueEntryCard({
@@ -20,6 +20,8 @@ function LeagueEntryCard({
   losses,
   wr,
 }: LeagueEntryCardProps) {
+  const isUnranked = !rank;
+
   return (
     <Flex
       bgColor={"secondary"}
@@ -33,33 +35,41 @@ function LeagueEntryCard({
       <CardTitle text={title} />
       {/* Contenido */}
       <Flex
-        // bgColor="red"
         direction={"row"}
         alignItems={"center"}
         height={"80px"}
         pl="15px"
         pr="10px"
+        justifyContent={"space-between"}
       >
-        <Image src={emblemUrl} height={12} ml={3} mt={1} />
-        {/* Rank container */}
-        <Flex ml={3} direction={"column"}>
+        {isUnranked ? (
           <Text fontSize="18px" fontWeight={600}>
-            {rank}
+            {title} - Unranked
           </Text>
-          <Text fontSize="14px" color={"ligthBlueText"}>
-            {leaguePoints} lp
-          </Text>
-        </Flex>
-        <Spacer />
-        {/* Winrate container */}
-        <Flex mr={3} pt={3} direction={"column"} textAlign={"right"}>
-          <Text fontSize="12px" fontWeight={600} color={"ligthBlueText"}>
-            {wins}W {losses}L
-          </Text>
-          <Text fontSize="12px" fontWeight={600} color={"ligthBlueText"}>
-            {wr}% Win Rate
-          </Text>
-        </Flex>
+        ) : (
+          <>
+            {emblemUrl && <Image src={emblemUrl} height={12} ml={3} mt={1} />}
+            {/* Rank container */}
+            <Flex ml={3} direction={"column"}>
+              <Text fontSize="18px" fontWeight={600}>
+                {rank}
+              </Text>
+              <Text fontSize="14px" color={"lightblueText"}>
+                {leaguePoints} lp
+              </Text>
+            </Flex>
+            <Spacer />
+            {/* Winrate container */}
+            <Flex mr={3} pt={3} direction={"column"} textAlign={"right"}>
+              <Text fontSize="12px" fontWeight={600} color={"lightblueText"}>
+                {wins}W {losses}L
+              </Text>
+              <Text fontSize="12px" fontWeight={600} color={"lightblueText"}>
+                {wr}% Win Rate
+              </Text>
+            </Flex>
+          </>
+        )}
       </Flex>
     </Flex>
   );
